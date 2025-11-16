f0f817310ac4412b39d18b6ca99bd2bc6e9d6a15
Ali1791 <alitarek13062005@gmail.com>
2025-11-16 23:29:28 +0200

    Add files via upload

---
+++ src/User/StudentRole.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+import Courses.Course;
+
+
+import Json.JsonCoursesReader;
+import java.util.List;
+/**
+ *
+ * @author AltAWKEl
+ */
+public class StudentRole {
+     public List<Course> viewCourses(Student s)
+   {JsonCoursesReader r =new JsonCoursesReader("courses.json");
+    List<Course> result=r.getCourses();
+   return result;  }
+     
+}
