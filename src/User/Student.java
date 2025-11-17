21b73bcfeb72de70375b1b75ab0d84e845fcecd7
Moaz Refaat <134746907+moaz91@users.noreply.github.com>
2025-11-17 03:16:32 +0200

    Add completedLessons functionality to Student class

--- src/User/Student.java
+++ src/User/Student.java
@@ -3,19 +3,23 @@ package User;
 import java.util.ArrayList;
 import java.util.List;
 import Json.JsonUserReader;
+
 public class Student extends User {
 
     private List<String> enrolledCourses;
+    private List<String> completedLessons;
 
     // Default constructor required by Gson
-    public Student(){
+    public Student() {
         super();
         this.enrolledCourses = new ArrayList<>();
+        this.completedLessons = new ArrayList<>();
     }
 
-    public Student(int id, String username,String email, String password, List<String> courses) {
-        super(id, username,email, password);
+    public Student(int id, String username, String email, String password, List<String> courses) {
+        super(id, username, email, password);
         this.enrolledCourses = courses;
+        this.completedLessons = new ArrayList<>();
     }
 
     public List<String> getEnrolledCourses() {
@@ -26,17 +30,20 @@ public class Student extends User {
         enrolledCourses.add(courseName);
     }
 
-    public void displayInfo() {
-      super.displayInfo();
-        System.out.println("Courses: " + enrolledCourses);
+    public List<String> getCompletedLessons() {
+        if (completedLessons == null) {
+            completedLessons = new ArrayList<>();
+        }
+        return completedLessons;
     }
-    
-    /*public static void main(String[] args) {
-        JsonUserReader r=new JsonUserReader("users.json");
-        List<Student> l=r.getStudents();
-        for(Student s: l)
-            s.displayInfo();
-        
-    }*/
-}
 
+    public void setCompletedLessons(List<String> completedLessons) {
+        this.completedLessons = completedLessons;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Courses: " + enrolledCourses);
+        System.out.println("Completed Lessons: " + (completedLessons != null ? completedLessons.size() : 0));
+    }
+}
